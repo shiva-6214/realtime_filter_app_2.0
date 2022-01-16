9935b7e188433a931b12ee9980fc620bcde58cd5
Smart Coder <69685153+shiva-6214@users.noreply.github.com>
2022-01-16 11:30:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+function preload() {
+
+}
+
+function setup() {
+    canvas = createCanvas(350, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(350, 300);
+    video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose", gotPoses);
+}
+
+function draw() {
+    image(video, 0, 0, 350, 300);
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!");
+}
+
+function gotPoses(results) {
+    if(results.length > 0) {
+        console.log(results);
+        console.log("nose x = " + results[0].pose.nose.x);
+        console.log("nose y = " + results[0].pose.nose.y);
+    }
+}
